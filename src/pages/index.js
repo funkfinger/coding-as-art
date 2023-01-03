83aad3135ff02c4c9cf217407b5928804b868aca
Jay Wiggins <5948+funkfinger@users.noreply.github.com>
2023-01-03 10:04:47 -0700

    add CaA2+ slides

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,14 @@ const IndexPage = () => {
       <h1>PCDS Coding as Art Information</h1>
       <ul>
         <li>
-          <a href="slides/first-days/index.html">First Days Slides</a>
+          <a href="slides/first-days-caa1/index.html">
+            First Days Slides - Coding as Art I
+          </a>
+        </li>
+        <li>
+          <a href="slides/first-days-caa2/index.html">
+            First Days Slides - Coding as Art II
+          </a>
         </li>
       </ul>
     </main>
