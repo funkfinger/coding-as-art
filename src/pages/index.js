61e2b7c0460904898ac683aeb901e51a8ebabf9f
Jay Wiggins <5948+funkfinger@users.noreply.github.com>
2023-01-02 22:26:25 -0700

    adding slides

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
 import * as React from "react";
 
 const IndexPage = () => {
-  return <main>PCDS Coding as Art Information</main>;
+  return (
+    <main>
+      <h1>PCDS Coding as Art Information</h1>
+      <ul>
+        <li>
+          <a href="slides/first-days/index.html">First Days Slides</a>
+        </li>
+      </ul>
+    </main>
+  );
 };
 
 export default IndexPage;
